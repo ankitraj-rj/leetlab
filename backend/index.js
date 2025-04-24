9b4337f3c0630b8a9372abba8625007a6d2e6fe5
Ankit Raj <ankitrajcollab@gmail.com>
2025-04-24 23:37:54 +0530

    add dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.listen(8080, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is Running on Port 8080");
 });
