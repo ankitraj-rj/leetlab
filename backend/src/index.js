231b59d29d456b7ba31d996b940f64f80f8cc40b
Ankit Raj <ankitrajcollab@gmail.com>
2025-05-12 10:23:50 +0530

    feat (backend) : add problems route

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(cookieParser()); 
 
 app.use("/api/v1/auth",authRoutes);
+app.use("/api/v1/problems",problemRoutes);
 
 app.get("/",(req,res)=>{
   res.send(" Hello Guys Welcome to leetlab 🔥 ")
